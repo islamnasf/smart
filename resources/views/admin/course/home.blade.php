54f65de24c0a35973389d268ae67367c9ff2d740
Ibrahem-Khaled <xx.vvv48@gmail.com>
2023-11-30 00:01:12 +0200

    solve

--- resources/views/admin/course/home.blade.php
+++ resources/views/admin/course/home.blade.php
@@ -32,41 +32,18 @@
         <!--=================================
  Main content -->
         <!-- main-content -->
-        <div class="content-wrapper">
 
+
+        <div class="content-wrapper">
 
             <div class="row">
                 <img src="{{ url('assets/images/teacher.jpg') }}"
-                    style="width:90%;  display: block; margin:30px; object-fit: contain; border-radius: 10px;"
+                    style="width:95%;  display: block; margin:30px; object-fit: contain; border-radius: 10px;"
                     alt="">
             </div>
+
             <!-- widgets -->
             <!-- widgets -->
-
-
-            <div class="col-xl-3 col-lg-6 col-md-6 mb-30">
-                <div class="card card-statistics h-100">
-                    <div class="card-body">
-                        <div class="clearfix">
-                            <div class="float-left">
-                                <span class="text-danger">
-                                    <i class=" fa fa-stop-circle highlight-icon" aria-hidden="true"
-                                        style="color:#175166; font-size:70px; padding-top:15px;padding-bottom:15px "></i>
-                                    <!-- <i class="fa fa-bar-chart-o highlight-icon" aria-hidden="true"></i> -->
-                                </span>
-                            </div>
-                            <div class="float-right text-center">
-                                <a href=''>
-                                    <p class="card-text text-dark" style="font-size: 27px; padding-top:15px ;">الكورسات
-                                    </p>
-                                </a>
-
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
 
             <div class="row">
                 <div class="col-xl-3 col-lg-6 col-md-6 mb-30">
@@ -75,69 +52,17 @@
                             <div class="clearfix">
                                 <div class="float-left">
                                     <span class="text-danger">
-                                        <a href="{{ route('addCourse') }}">
-                                            <i class=" fa fa-plus-circle highlight-icon" aria-hidden="true"
-                                                style="color:#175166; font-size:70px;"></i>
-                                        </a>
+                                        <i class=" fa fa-stop-circle highlight-icon" aria-hidden="true"
+                                            style="color:#175166; font-size:70px; padding-top:15px;padding-bottom:15px "></i>
                                         <!-- <i class="fa fa-bar-chart-o highlight-icon" aria-hidden="true"></i> -->
                                     </span>
                                 </div>
                                 <div class="float-right text-center">
-                                    <a href="{{ route('addCourse') }}">
-                                        <p class="card-text text-dark "
-                                            style="font-size: 22px;  font-weight: bolder ;padding: 5px;">
-                                            اضافة كورس
+                                    <a href=''>
+                                        <p class="card-text text-dark" style="font-size: 27px; padding-top:15px ;">
+                                            الكورسات</p>
                                     </a>
-                                    </p>
-                                </div>
-                            </div>
 
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-lg-6 col-md-6 mb-30">
-                    <div class="card card-statistics h-100">
-                        <div class="card-body">
-                            <div class="clearfix" style="padding: 20px;">
-                                <div class="float-left">
-                                    <span class="text-danger">
-                                        <a href="{{ route('addCourse') }}">
-                                            <i class=" fa fa-video-camera highlight-icon" aria-hidden="true"
-                                                style="color:#175166; font-size:70px;"></i>
-                                        </a>
-                                        <!-- <i class="fa fa-bar-chart-o highlight-icon" aria-hidden="true"></i> -->
-                                    </span>
-                                </div>
-                                <div class="float-right text-center">
-                                    <a href="{{ route('addCourse') }}">
-                                        <p class="card-text text-dark "
-                                            style="font-size: 22px;  font-weight: bolder ;padding: 5px;">
-                                            الكورسات المفعلة</p>
-                                    </a>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-xl-3 col-lg-6 col-md-6 mb-30">
-                    <div class="card card-statistics h-100">
-                        <div class="card-body">
-                            <div class="clearfix">
-                                <div class="float-left">
-                                    <span class="text-danger">
-                                        <i class=" fa fa-archive highlight-icon" aria-hidden="true"
-                                            style="color:#175166; font-size:70px;"></i>
-                                        <!-- <i class="fa fa-bar-chart-o highlight-icon" aria-hidden="true"></i> -->
-                                    </span>
-                                </div>
-                                <div class="float-right text-center">
-                                    <a href='{{ route('course') }}'>
-                                        <p class="card-text text-dark "
-                                            style="font-size: 22px;  font-weight: bolder ;padding: 5px;">
-                                            أرشيف الكورس</p>
-                                    </a>
                                 </div>
                             </div>
 
@@ -150,15 +75,63 @@
                             <div class="clearfix">
                                 <div class="float-left">
                                     <span class="text-danger">
-                                        <i class=" fa fa-subscript  highlight-icon" aria-hidden="true"
-                                            style="color:#175166; font-size:70px;"></i>
+                                        <i class=" fa fa-stop-circle highlight-icon" aria-hidden="true"
+                                            style="color:#175166; font-size:70px; padding-top:15px;padding-bottom:15px "></i>
                                         <!-- <i class="fa fa-bar-chart-o highlight-icon" aria-hidden="true"></i> -->
                                     </span>
                                 </div>
                                 <div class="float-right text-center">
-                                    <p class="card-text text-dark "
-                                        style="font-size: 22px;  font-weight: bolder ;padding: 5px;">
-                                        الباقات</p>
+                                    <a href=''>
+                                        <p class="card-text text-dark" style="font-size: 27px; padding-top:15px ;">
+                                            الكورسات</p>
+                                    </a>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-6 col-md-6 mb-30">
+                    <div class="card card-statistics h-100">
+                        <div class="card-body">
+                            <div class="clearfix">
+                                <div class="float-left">
+                                    <span class="text-danger">
+                                        <i class=" fa fa-stop-circle highlight-icon" aria-hidden="true"
+                                            style="color:#175166; font-size:70px; padding-top:15px;padding-bottom:15px "></i>
+                                        <!-- <i class="fa fa-bar-chart-o highlight-icon" aria-hidden="true"></i> -->
+                                    </span>
+                                </div>
+                                <div class="float-right text-center">
+                                    <a href=''>
+                                        <p class="card-text text-dark" style="font-size: 27px; padding-top:15px ;">
+                                            الكورسات</p>
+                                    </a>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-6 col-md-6 mb-30">
+                    <div class="card card-statistics h-100">
+                        <div class="card-body">
+                            <div class="clearfix">
+                                <div class="float-left">
+                                    <span class="text-danger">
+                                        <i class=" fa fa-stop-circle highlight-icon" aria-hidden="true"
+                                            style="color:#175166; font-size:70px; padding-top:15px;padding-bottom:15px "></i>
+                                        <!-- <i class="fa fa-bar-chart-o highlight-icon" aria-hidden="true"></i> -->
+                                    </span>
+                                </div>
+                                <div class="float-right text-center">
+                                    <a href=''>
+                                        <p class="card-text text-dark" style="font-size: 27px; padding-top:15px ;">
+                                            الكورسات</p>
+                                    </a>
+
                                 </div>
                             </div>
 
@@ -175,63 +148,16 @@
                             <div class="clearfix">
                                 <div class="float-left">
                                     <span class="text-danger">
-                                        <i class=" fa fa-book  highlight-icon" aria-hidden="true"
-                                            style="color:#175166; font-size:70px;"></i>
+                                        <i class=" fa fa-stop-circle highlight-icon" aria-hidden="true"
+                                            style="color:#175166; font-size:70px; padding-top:15px;padding-bottom:15px "></i>
                                         <!-- <i class="fa fa-bar-chart-o highlight-icon" aria-hidden="true"></i> -->
                                     </span>
                                 </div>
                                 <div class="float-right text-center">
-                                    <p class="card-text text-dark"
-                                        style="font-size: 22px;  font-weight: bolder ;padding: 5px;">اشتراكات
-                                        الطلبة
-                                    </p>
-
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-lg-6 col-md-6 mb-30">
-                    <div class="card card-statistics h-100">
-                        <div class="card-body">
-                            <div class="clearfix">
-                                <div class="float-left">
-                                    <span class="text-danger">
-
-                                        <i class=" fa fa-flag highlight-icon" aria-hidden="true"
-                                            style="color:#175166; font-size:70px;"></i>
-                                        <!-- <i class="fa fa-bar-chart-o highlight-icon" aria-hidden="true"></i> -->
-                                    </span>
-                                </div>
-                                <div class="float-right text-center">
-                                    <a href="{{ route('getContact') }}">
-                                        <p class="card-text text-dark"
-                                            style="font-size: 22px;  font-weight: bolder ;padding: 5px;">
-                                            التقرير كامل</p>
+                                    <a href=''>
+                                        <p class="card-text text-dark" style="font-size: 27px; padding-top:15px ;">
+                                            الكورسات</p>
                                     </a>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-xl-3 col-lg-6 col-md-6 mb-30">
-                    <div class="card card-statistics h-100">
-                        <div class="card-body">
-                            <div class="clearfix">
-                                <div class="float-left">
-                                    <span class="text-danger">
-                                        <i class="fa fa-users highlight-icon" aria-hidden="true"
-                                            style="color:#175166; font-size:70px;"></i>
-                                        <!-- <i class="fa fa-bar-chart-o highlight-icon" aria-hidden="true"></i> -->
-                                    </span>
-                                </div>
-                                <div class="float-right text-center">
-                                    <p class="card-text text-dark"
-                                        style="font-size: 22px;  font-weight: bolder ;padding: 5px;">
-                                        الاشتراكات المنتهية</p>
 
                                 </div>
                             </div>
@@ -245,15 +171,16 @@
                             <div class="clearfix">
                                 <div class="float-left">
                                     <span class="text-danger">
-                                        <i class=" fa fa-cogs  highlight-icon" aria-hidden="true"
-                                            style="color:#175166; font-size:70px;"></i>
+                                        <i class=" fa fa-stop-circle highlight-icon" aria-hidden="true"
+                                            style="color:#175166; font-size:70px; padding-top:15px;padding-bottom:15px "></i>
                                         <!-- <i class="fa fa-bar-chart-o highlight-icon" aria-hidden="true"></i> -->
                                     </span>
                                 </div>
                                 <div class="float-right text-center">
-                                    <p class="card-text text-dark"
-                                        style="font-size: 22px;  font-weight: bolder ;padding: 5px;">ارشيف
-                                        الباقات</p>
+                                    <a href=''>
+                                        <p class="card-text text-dark" style="font-size: 27px; padding-top:15px ;">
+                                            الكورسات</p>
+                                    </a>
 
                                 </div>
                             </div>
@@ -261,6 +188,55 @@
                         </div>
                     </div>
                 </div>
+                <div class="col-xl-3 col-lg-6 col-md-6 mb-30">
+                    <div class="card card-statistics h-100">
+                        <div class="card-body">
+                            <div class="clearfix">
+                                <div class="float-left">
+                                    <span class="text-danger">
+                                        <i class=" fa fa-stop-circle highlight-icon" aria-hidden="true"
+                                            style="color:#175166; font-size:70px; padding-top:15px;padding-bottom:15px "></i>
+                                        <!-- <i class="fa fa-bar-chart-o highlight-icon" aria-hidden="true"></i> -->
+                                    </span>
+                                </div>
+                                <div class="float-right text-center">
+                                    <a href=''>
+                                        <p class="card-text text-dark" style="font-size: 27px; padding-top:15px ;">
+                                            الكورسات</p>
+                                    </a>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-6 col-md-6 mb-30">
+                    <div class="card card-statistics h-100">
+                        <div class="card-body">
+                            <div class="clearfix">
+                                <div class="float-left">
+                                    <span class="text-danger">
+                                        <i class=" fa fa-stop-circle highlight-icon" aria-hidden="true"
+                                            style="color:#175166; font-size:70px; padding-top:15px;padding-bottom:15px "></i>
+                                        <!-- <i class="fa fa-bar-chart-o highlight-icon" aria-hidden="true"></i> -->
+                                    </span>
+                                </div>
+                                <div class="float-right text-center">
+                                    <a href=''>
+                                        <p class="card-text text-dark" style="font-size: 27px; padding-top:15px ;">
+                                            الكورسات</p>
+                                    </a>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+
+
             </div>
 
             <!--=================================
