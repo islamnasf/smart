f589e15e2d02cac9b1b52f0c7bccc79af9eaf106
Ibrahem-Khaled <xx.vvv48@gmail.com>
2023-11-30 11:19:23 +0200

    edit home ui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,134 +1,288 @@
+{{-- href="{{ URL::asset('css/headers.css') }}" --}}
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ URL::asset('css/headers.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{ URL::asset('css/slide.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{ URL::asset('css/footer.css') }}" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css"
-        integrity="sha384-4LISF5TTJX/fLmGSxO53rV4miRxdg84mZsxmO8Rx5jGtp/LbrixFETvWa5a6sESd" crossorigin="anonymous">
-    <title>Home</title>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>FlexStart Bootstrap Template - Index</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
 </head>
 
 <body>
+    <!-- ======= Header ======= -->
+    <header id="header" class="header fixed-top">
+        <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
 
-    <div class="main">
-        <div class="headMain">
-            <div class="itemHeader">
-                <i class="bi bi-envelope" style="font-size: 2rem; color: rgb(64, 226, 255);"></i>
-                <div style="margin: 10px;">
-                    <h4>البريد الالكتروني</h4>
-                    <a href="#" class="contact">any@any.com</a>
+            <a href="{{ route('home') }}" class="logo d-flex align-items-center">
+                <img src="assets/img/logo.png" alt="">
+                <span>Education</span>
+            </a>
+
+            <nav id="navbar" class="navbar">
+                <ul>
+                    @if (Auth::check())
+                        <li><a class="nav-link scrollto" style="color: #bb0852" href="{{ route('logout') }}">تسجيل الخروج</a></li>
+                    @endif
+                    <li><a class="nav-link scrollto" href="{{ route('contactus') }}">اسئل الادارة</a></li>
+                    <li><a class="nav-link scrollto" href="#team">بنك الاسئلة</a></li>
+                    <li><a href="#">الدذكرات</a></li>
+                    <li><a class="nav-link scrollto" href="#contact">استكشاف المواد</a></li>
+                    <li><a class="nav-link scrollto active" href="#hero">الرئيسية</a></li>
+                    @if (Auth::check())
+                        <li><a class="getstarted scrollto"
+                                href="{{ route('getProfile') }}">{{ Auth()->user()->name }}</a>
+                        </li>
+                    @else
+                        <li><a class="getstarted scrollto" href="{{ route('login') }}">انضم الينا الان</a></li>
+                    @endif
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav><!-- .navbar -->
+
+        </div>
+    </header><!-- End Header -->
+
+    <!-- ======= Hero Section ======= -->
+    <section id="hero" class="hero d-flex align-items-center">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 d-flex flex-column justify-content-center">
+                    <h1 data-aos="fade-up">نحن نقدم حلولاً حديثة لتنميتك</h1>
+                    <h2 data-aos="fade-up" data-aos-delay="400">يمكنك الحصول علي اختبارات سابقة واختبارات قصيرة وبنوك
+                        حديثة
+                    </h2>
+                </div>
+                <div class="col-lg-6 hero-img" data-aos="zoom-out" data-aos-delay="200">
+                    <img src="assets/img/hero-img.png" class="img-fluid" alt="">
                 </div>
             </div>
-            <div class="itemHeader">
-                <div style="margin: 10px;">
-                    <h1>Education</h1>
+        </div>
+
+    </section><!-- End Hero -->
+
+    <main id="main">
+        <!-- ======= About Section ======= -->
+        <section id="about" class="about">
+
+            <div class="container" data-aos="fade-up">
+                <div class="row gx-0">
+
+                    <div class="col-lg-6 d-flex flex-column justify-content-center" data-aos="fade-up"
+                        data-aos-delay="200">
+                        <div class="content">
+                            <h3>نبذة عن المنصة</h3>
+                            <h2> منصة عربية تعليمية لتعلم عبر الانترنت </h2>
+                            <p>
+                                Quisquam vel ut sint cum eos hic dolores aperiam. Sed deserunt et. Inventore et et dolor
+                                consequatur
+                                itaque ut voluptate sed et. Magnam nam ipsum tenetur suscipit voluptatum nam et est
+                                corrupti.
+                            </p>
+                            <div class="text-center text-lg-start">
+                                <a href="#"
+                                    class="btn-read-more d-inline-flex align-items-center justify-content-center align-self-center">
+                                    <span>قراءة المزيد</span>
+                                    <i class="bi bi-arrow-right"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="200">
+                        <img src="assets/img/about.jpg" class="img-fluid" alt="">
+                    </div>
+
                 </div>
             </div>
-            <div class="itemHeader">
-                <i class="bi bi-telephone-fill" style="font-size: 2rem; color: rgb(64, 226, 255);"></i>
-                <div style="margin: 10px;">
-                    <h4>البريد الالكتروني</h4>
-                    <a href="#" class="contact">any@any.com</a>
+
+        </section><!-- End About Section -->
+
+        <!-- ======= Values Section ======= -->
+        <section id="values" class="values">
+
+            <div class="container" data-aos="fade-up">
+
+                <header class="section-header">
+                    <h2>يمكنك الإنضمام لنخبة من أقوي معلمي دولة الكويت</h2>
+                    <p>معنا فالك التفوق</p>
+                </header>
+
+                <div class="row">
+
+                    <div class="col-lg-3" data-aos="fade-up" data-aos-delay="200">
+                        <div class="box">
+                            <img src="https://cdn-icons-png.flaticon.com/128/7359/7359501.png" class="img-fluid"
+                                alt="">
+                            <h3>كورسات اونلاين مباشر</h3>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3" data-aos="fade-up" data-aos-delay="200">
+                        <div class="box">
+                            <img src="https://cdn-icons-png.flaticon.com/128/3208/3208933.png" class="img-fluid"
+                                alt="">
+                            <h3>اختبارات سابقة وبنوك</h3>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="400">
+                        <div class="box">
+                            <img src="https://cdn-icons-png.flaticon.com/128/9165/9165323.png" class="img-fluid"
+                                alt="">
+                            <h3>مذكرات Education</h3>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="600">
+                        <div class="box">
+                            <img src="https://cdn-icons-png.flaticon.com/128/2997/2997554.png" class="img-fluid"
+                                alt="">
+                            <h3>كورسات مسجلة</h3>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </section><!-- End Values Section -->
+
+        <!-- ======= Counts Section ======= -->
+        <section id="counts" class="counts">
+            <div class="container" data-aos="fade-up">
+
+                <div class="row gy-4">
+
+                    <div class="col-lg-3 col-md-6">
+                        <div class="count-box">
+                            <i class="bi bi-emoji-smile"></i>
+                            <div>
+                                <span data-purecounter-start="0" data-purecounter-end="232"
+                                    data-purecounter-duration="1" class="purecounter"></span>
+                                <p>عملاء سعداء</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6">
+                        <div class="count-box">
+                            <i class="bi bi-journal-richtext" style="color: #ee6c20;"></i>
+                            <div>
+                                <span data-purecounter-start="0" data-purecounter-end="521"
+                                    data-purecounter-duration="1" class="purecounter"></span>
+                                <p>المشاريع</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6">
+                        <div class="count-box">
+                            <i class="bi bi-headset" style="color: #15be56;"></i>
+                            <div>
+                                <span data-purecounter-start="0" data-purecounter-end="1463"
+                                    data-purecounter-duration="1" class="purecounter"></span>
+                                <p>ساعات من الدعم</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6">
+                        <div class="count-box">
+                            <i class="bi bi-people" style="color: #bb0852;"></i>
+                            <div>
+                                <span data-purecounter-start="0" data-purecounter-end="15"
+                                    data-purecounter-duration="1" class="purecounter"></span>
+                                <p>مجدون في العمل</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </section><!-- End Counts Section -->
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer" class="footer">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row gy-4">
+                    <div class="col-lg-5 col-md-12 footer-info">
+                        <a href="index.html" class="logo d-flex align-items-center">
+                            <img src="assets/img/logo.png" alt="">
+                            <span>Education</span>
+                        </a>
+                        <p> افضل منصة تعليمية عربية يمكنك الحصول منها علي افضل الكورسات لتعلم عبر الانترنت</p>
+                        <div class="social-links mt-3">
+                            <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+                            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+                            <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+                        <h4>اتصل بنا</h4>
+                        <p>
+                            A108 Adam Street <br>
+                            New York, NY 535022<br>
+                            United States <br><br>
+                            <strong>رقم الهاتف:</strong> +1 5589 55488 55<br>
+                            <strong>الايميل:</strong> info@example.com<br>
+                        </p>
+
+                    </div>
+
                 </div>
             </div>
         </div>
 
-        <div class="header-bar">
-            <a href="{{route('contactus')}}">اسئل الادارة</a>
-            <a href="{{route('login')}}">تسجيل الدخول</a>
-            <a href="#">بنك الاسئلة</a>
-            <a href="#">المذكرات</a>
-            <a href="#">استكشف المواد</a>
-            <a href="{{route('home')}}">الرئيسية</a>
+        <div class="container">
+            <div class="copyright">
+                &copy; Copyright <strong><span>Education</span></strong>. All Rights Reserved
+            </div>
         </div>
+    </footer><!-- End Footer -->
 
-        <div class="slideshow-container">
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
 
-            <div class="mySlides fade">
-                <div class="numbertext">1 / 3</div>
-                <img src="https://images.unsplash.com/photo-1543269865-cbf427effbad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c3R1ZGVudHN8ZW58MHx8MHx8fDA%3D"
-                    style="width:100%;height: 500px; object-fit: contain; background-color: #505050;border-radius: 10px;">
-                <div class="text">Caption Text</div>
-            </div>
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
 
-            <div class="mySlides fade">
-                <div class="numbertext">2 / 3</div>
-                <img src="https://images.unsplash.com/photo-1571260899304-425eee4c7efc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c3R1ZGVudHN8ZW58MHx8MHx8fDA%3D"
-                    style="width:100%;height: 500px; object-fit: contain; background-color: #505050;border-radius: 10px;">
-                <div class="text">Caption Two</div>
-            </div>
-
-            <div class="mySlides fade">
-                <div class="numbertext">3 / 3</div>
-                <img src="https://images.unsplash.com/photo-1682687220777-2c60708d6889?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwzMXx8fGVufDB8fHx8fA%3D%3D"
-                    style="width:100%;height: 500px; object-fit: contain; background-color: #505050;border-radius: 10px;">
-                <div class="text">Caption Three</div>
-            </div>
-
-            <a class="prev" onclick="plusSlides(-1)">❮</a>
-            <a class="next" onclick="plusSlides(1)">❯</a>
-
-        </div>
-        <br>
-
-        <div style="text-align:center">
-            <span class="dot" onclick="currentSlide(1)"></span>
-            <span class="dot" onclick="currentSlide(2)"></span>
-            <span class="dot" onclick="currentSlide(3)"></span>
-        </div>
-
-
-        <footer class="footer">
-            <div class="contact-info">
-                <h3>Contact Us</h3>
-                <p>123 Main Street, City</p>
-                <p>Email: info@example.com</p>
-                <p>Phone: 123-456-7890</p>
-            </div>
-
-            <div class="social-media">
-                <a href="#" target="_blank"><i class="bi bi-facebook"
-                        style="font-size: 2rem; color: rgb(0, 26, 255);"></i></a>
-                <a href="#" target="_blank"><i class="bi bi-instagram"
-                        style="font-size: 2rem; color: rgb(193, 36, 255);"></i></a>
-                <a href="#" target="_blank"><i class="bi bi-whatsapp"
-                        style="font-size: 2rem; color: rgb(50, 255, 60);"></i></a>
-            </div>
-        </footer>
-
-
-        <script>
-            let slideIndex = 1;
-            showSlides(slideIndex);
-
-            function plusSlides(n) {
-                showSlides(slideIndex += n);
-            }
-
-            function currentSlide(n) {
-                showSlides(slideIndex = n);
-            }
-
-            function showSlides(n) {
-                let i;
-                let slides = document.getElementsByClassName("mySlides");
-                let dots = document.getElementsByClassName("dot");
-                if (n > slides.length) { slideIndex = 1 }
-                if (n < 1) { slideIndex = slides.length }
-                for (i = 0; i < slides.length; i++) {
-                    slides[i].style.display = "none";
-                }
-                for (i = 0; i < dots.length; i++) {
-                    dots[i].className = dots[i].className.replace(" active", "");
-                }
-                slides[slideIndex - 1].style.display = "block";
-                dots[slideIndex - 1].className += " active";
-            }
-        </script>
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
 
 </body>
 
