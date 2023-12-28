a9d702a6f068a5ce74cd614c1c879c06b6f5dacb
Ibrahem-Khaled <xx.vvv48@gmail.com>
2023-12-28 11:26:37 +0200

    edit in cart items frontend

--- resources/views/student/cart.blade.php
+++ resources/views/student/cart.blade.php
@@ -7,122 +7,21 @@
 @endsection
 @section('page-header')
 <style>
-    /* أسلوب التنسيق */
-    .body {
-        font-family: 'Arial', sans-serif;
-        background-color: #f4f4f4;
-        margin: 0;
-        padding: 0;
-        display: flex;
-        max-height: 80vh;
-        margin: 0px auto;
-    }
+    .gradient-custom {
+        /* fallback for old browsers */
+        background: #6a11cb;
 
-    .custom-div {
-        background-color: #175166;
-        border: 1px solid #175166;
-        display: flex;
-        padding: 20px;
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-        width: 250px;
-        border-radius: 8px;
-        position: relative;
-        height: 150px;
-        margin: 10px;
+        /* Chrome 10-25, Safari 5.1-6 */
+        background: -webkit-linear-gradient(to right, rgb(255, 255, 255), rgb(231, 231, 231));
 
-    }
-
-    .custom-div h2 {
-        margin: 30px 50% 5px 5px;
-        color: #f4f4f4;
-        font-size: 35px;
-    }
-
-    .custom-div p {
-        color: #f4f4f4;
-        margin-right: 60px;
-        font-size: 25px;
-
-    }
-
-    .close-btn {
-        position: absolute;
-        top: 10px;
-        right: 10px;
-        background-color: transparent;
-        color: #fff;
-        border: none;
-        padding: 5px 12px;
-        border-radius: 4px;
-        cursor: pointer;
-        font-size: 20px;
-    }
-
-    @media screen and (max-width: 600px) {
-        .body {
-            font-family: 'Arial', sans-serif;
-            background-color: #f4f4f4;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            flex-direction: column;
-            height: 50vh;
-            margin: 0px auto;
-        }
-
-        .custom-div {
-            width: 280px;
-            margin: 10px auto;
-        }
-    }
-
-    .imgclose:hover {
-        opacity: .9;
-    }
-
-    .donepay:hover {
-        background-color: #444;
-        box-shadow: 1px 1px 1px 2px black;
-        opacity: .8;
-
-
-
+        /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+        background: linear-gradient(to right, rgb(255, 255, 255), rgb(231, 231, 231))
     }
 </style>
-<div>
-    <h2
-        style="text-align: center; margin: 10px; background-color: #444; padding: 10px; color: #fff; border-radius: 5px;">
-        اجمالي المشتريات </h2>
-</div>
+{{-- 
 <div class="body">
-    @foreach ($cart as $cart)
-        <div class="custom-div">
-            <form action="{{ route('studentCartDelete', $cart->id) }}" method="post">
-                @csrf
-                <button class="close-btn" onclick="closeDiv(this)"><img
-                        src="https://cdn-icons-png.flaticon.com/128/458/458594.png" width="28px"
-                        class="imgclose"></button>
-            </form>
-            <div style="flex-direction: column;">
-                <h2>{{ $cart->course->subject_name }}</h2>
-                <p> سعر : {{ $cart->price }} د.ك</p>
-            </div>
-        </div>
-    @endforeach
-
-    <script>
-        function closeDiv(button) {
-            // الحصول على العنصر الأب (custom-div) للزر الذي تم النقر عليه
-            var divElement = button.closest('.custom-div');
-
-            // إخفاء العنصر
-            if (divElement) {
-                divElement.style.display = 'none';
-            }
-        }
-    </script>
-</div>
-<form action="{{ route('studentPostOrder') }}" method="POST">
+</div> --}}
+{{-- <form action="{{ route('studentPostOrder') }}" method="POST">
     @csrf
     <div>
         <h2 style="text-align: center; background-color:#bbb; padding: 8px;"> الاجمالي : {{ $sumPrice }} د.ك</h2>
@@ -145,7 +44,117 @@
             style="width: 100%;  border: 0px; text-align: center; background-color:#085166; padding: 6px; color: #fff; font-size: 28px; margin: 3px; opacity: 1;"
             class="donepay">اتمام عملية الدفع</button>
     </div>
-</form>
+</form> --}}
+
+<div>
+    <h2
+        style="text-align: center; margin: 10px; background-color: #444; padding: 10px; color: #fff; border-radius: 5px;">
+        سلة مشترياتك </h2>
+</div>
+<section class="h-100 gradient-custom">
+    <div class="container py-5">
+        <div class="row d-flex justify-content-center my-4">
+            <div class="col-md-8">
+                <div class="card mb-4">
+                    <div class="card-header py-3">
+                        <h5 class="mb-0">عدد الكورسات في السلة {{ $cart->count() }} </h5>
+                    </div>
+                    <div class="card-body">
+                        <!-- Single item -->
+                        @foreach ($cart as $cart)
+                            <div
+                                style="display: flex; justify-content: space-around;flex-direction: row;align-items: center">
+                                <div class="col-lg-3 col-sm-12 mb-4 mb-lg-0">
+                                    <!-- Image -->
+                                    <div class="bg-image hover-overlay hover-zoom ripple rounded"
+                                        data-mdb-ripple-color="light">
+                                        <img src="{{ asset('cartImage.jpeg') }}" width="140px"
+                                            style="border-radius: 10px" alt="image" />
+                                    </div>
+                                    <!-- Image -->
+                                </div>
+
+                                <div class="col-lg-4 col-sm-12 mb-4 mb-lg-0">
+                                    <!-- Data -->
+                                    <p><strong>{{ $cart->course->subject_name }}</strong></p>
+                                    <p>{{ $cart->course->classroom }}</p>
+                                    <p class="text-start text-md-left pt-4">
+                                        <strong> سعر : {{ $cart->price }} د.ك</strong>
+
+                                    </p>
+                                    <!-- Data -->
+                                </div>
+                                <div class="col-lg-4 text-right col-sm-12 mb-4 mb-lg-0">
+                                    <!-- Price -->
+                                  
+                                    <form action="{{ route('studentCartDelete', $cart->id) }}" method="post">
+                                        @csrf
+                                        <button type="submit" style="border: none ; background:none"
+                                            data-mdb-toggle="tooltip" title="Remove item">
+                                            <img src="https://cdn-icons-png.flaticon.com/128/6711/6711573.png"
+                                                width="40px" />
+                                        </button>
+                                    </form>
+                                    <!-- Price -->
+                                </div>
+                            </div>
+                            <hr class="my-4" />
+                        @endforeach
+                        <!-- Single item -->
+                    </div>
+                </div>
+                <div class="card mb-4 mb-lg-0">
+                    <div class="card-body">
+                        <p><strong>وسائل الدفع لدينا</strong></p>
+                        <img class="me-2" width="45px"
+                            src="https://mdbcdn.b-cdn.net/wp-content/plugins/woocommerce-gateway-stripe/assets/images/visa.svg"
+                            alt="Visa" />
+                        <img class="me-2" width="45px"
+                            src="https://mdbcdn.b-cdn.net/wp-content/plugins/woocommerce-gateway-stripe/assets/images/amex.svg"
+                            alt="American Express" />
+                        <img class="me-2" width="45px"
+                            src="https://mdbcdn.b-cdn.net/wp-content/plugins/woocommerce-gateway-stripe/assets/images/mastercard.svg"
+                            alt="Mastercard" />
+                    </div>
+                </div>
+            </div>
+            <form class="col-md-4" action="{{ route('studentPostOrder') }}" method="POST">
+                @csrf
+
+                <div class="card mb-4">
+                    <div class="card-header py-3">
+                        <h5 class="mb-0">ملخص</h5>
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group list-group-flush">
+                            <li
+                                class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
+                                الكورسات
+                                <span>{{ $sumPrice }}</span>
+                            </li>
+                            <li
+                                class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
+                                <div>
+                                    <strong>المبلغ الإجمالي</strong>
+                                    <strong>
+                                        <p class="mb-0">(بما في ذلك ضريبة القيمة المضافة)</p>
+                                    </strong>
+                                </div>
+                                <span><strong>الاجمالي : {{ $sumPrice }} د.ك</strong></span>
+                            </li>
+                        </ul>
+
+                        <button type="submit" class="btn btn-primary btn-lg btn-block">
+                            اكمال عملية الشراء
+                        </button>
+                    </div>
+                </div>
+
+            </form>
+        </div>
+    </div>
+</section>
+
 <!-- row closed -->
 @endsection
 @section('js')
